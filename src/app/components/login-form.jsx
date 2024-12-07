51425beb5c608d3d560b7328c294cd0aece0980f
claudia <claudiaelena091@gmail.com>
2024-12-07 12:32:56 -0400

    segundo avance, login

--- src/app/components/login-form.jsx
+++ src/app/components/login-form.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { GithubIcon } from 'lucide-react'
+import Image from 'next/image'
 
 export default function LoginForm() {
   const [email, setEmail] = useState('')
@@ -18,9 +18,16 @@ export default function LoginForm() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8">
-        <div>
+        <div className="flex flex-col items-center">
+        <Image
+          src="/Logo_UDO.svg.png"
+          alt="Logo_UDO"
+          width={150}
+          height={150}
+          className="mb-4"
+        />
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Inicia sesión en tu cuenta
+            Control de Postgrado <br/> Ciencias Administrativas
           </h2>
         </div>
         <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
@@ -36,7 +43,7 @@ export default function LoginForm() {
                 autoComplete="email"
                 required
                 className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-                placeholder="Correo electrónico"
+                placeholder="Usuario"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
@@ -68,38 +75,6 @@ export default function LoginForm() {
             </Button>
           </div>
         </form>
-        <div className="mt-6">
-          <div className="relative">
-            <div className="absolute inset-0 flex items-center">
-              <div className="w-full border-t border-gray-300"></div>
-            </div>
-            <div className="relative flex justify-center text-sm">
-              <span className="bg-gray-50 px-2 text-gray-500">O continúa con</span>
-            </div>
-          </div>
-
-          <div className="mt-6 grid grid-cols-2 gap-3">
-            <div>
-              <Button
-                className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
-              >
-                <span className="sr-only">Iniciar sesión con Google</span>
-                <svg className="h-5 w-5" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-                  <path d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z" />
-                </svg>
-              </Button>
-            </div>
-
-            <div>
-              <Button
-                className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
-              >
-                <span className="sr-only">Iniciar sesión con GitHub</span>
-                <GithubIcon className="h-5 w-5" />
-              </Button>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   )
